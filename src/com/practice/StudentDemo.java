29aeabddf3e90b790e069c4b446a37e549541592
sgw <384840894@qq.com>
2018-12-05 18:07:53 +0800

    day 9 String and StringBuilder

--- src/com/practice/StudentDemo.java
+++ src/com/practice/StudentDemo.java
@@ -11,7 +11,16 @@ public class StudentDemo {
 		Student stu2 = new Student(30,"Tom");
 		System.out.println("stu2-name:"+stu2.name()+" stu2-age:"+stu2.age());
 		
+		Student[] stuArr = new Student[3];
+		Student stu3 = new Student(35,"join");
+		stuArr[0] = stu1;
+		stuArr[1] = stu2;
+		stuArr[2] = stu3;
 		
+		for (int i=0;i<stuArr.length;i++) {
+			Student s = stuArr[i];
+			System.out.println("第"+i+"个学生的姓名及年龄："+s.name()+" "+s.age());
+		}
 	}
 
 }
