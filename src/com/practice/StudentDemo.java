a800393bcf88cbeb7741fcec00eea978a34a648e
sgw <384840894@qq.com>
2018-12-07 17:54:27 +0800

    IO练习

--- src/com/practice/StudentDemo.java
+++ src/com/practice/StudentDemo.java
@@ -1,9 +1,10 @@
 package com.practice;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
 
 public class StudentDemo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException  {
 		Student stu1 = new Student();
 //		System.out.println("stu1-name:"+stu1.name()+" stu1-age:"+stu1.age());
 		stu1.setAge(26);
@@ -31,6 +32,12 @@ public class StudentDemo {
 		stuArr.add(stu2);
 		stuArr.add(stu3);
 		
+		String srcFile = "src.txt";
+		String descFile = "desc.txt";
+		method1(srcFile,descFile);
+		
+		
+		
 		while(true) {
 			System.out.println("---学生管理系统---");
 			System.out.println("1.查看所有学生");
@@ -55,17 +62,19 @@ public class StudentDemo {
 				changeStudent(stuArr);
 				break;
 			case "5":
-				System.exit(0);
+				System.out.println("系统已退出");
+				exitAndSave(stuArr);
 				break;
 			default:
-				System.out.println("输入有误，系统已退出");
-				System.exit(0);
+				System.out.println("系统已退出");
+				exitAndSave(stuArr);
 				break;
 			}
 		}
 				
 	}
 	
+	//查找学生
 	public static void findAllStudent(ArrayList<Student> stuArr) {
 		if(stuArr.size() == 0) {
 			System.out.println("当前系统没有学生");
@@ -79,6 +88,8 @@ public class StudentDemo {
 		}
 		
 	}
+	
+	//添加学生
 	
 	public static void addStudent(ArrayList<Student> stuArr) {
 		Scanner sc;
@@ -113,7 +124,10 @@ public class StudentDemo {
 		
 		Student newStu = new Student(nameStr,age);
 		stuArr.add(newStu);
+		
 	}
+	
+	//移除学生
 	
 	public static void removeStudent(ArrayList<Student> stuArr) {
 		System.out.println("请输入要删除第几个学生");
@@ -134,6 +148,8 @@ public class StudentDemo {
 		stuArr.remove(--x);
 		
 	}
+	
+	//修改学生
 	
 	public static void changeStudent(ArrayList<Student> stuArr) {
 		
@@ -185,4 +201,49 @@ public class StudentDemo {
 		
 	}
 
+	//保存数据
+	
+	public static void exitAndSave(ArrayList<Student> stuArr) throws IOException{
+		BufferedWriter bw = new BufferedWriter(new FileWriter("stuArr.txt"));
+		for(int i=0;i<stuArr.size();i++) {
+			Student str = stuArr.get(i);
+			StringBuilder sb = new StringBuilder();
+			sb.append(str.name()).append(",").append(str.age());
+			bw.write(sb.toString());
+			bw.newLine();
+			bw.flush();
+		}
+		bw.close();
+		System.exit(0);
+	}
+	
+	//IO缓冲流
+	public static void method1(String srcFile,String descFile) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(srcFile));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(descFile));
+		
+		//一次读取一行字符串
+		String line;
+		while((line = br.readLine()) != null) {
+			bw.write(line);
+			bw.newLine();
+			bw.flush();
+		}
+		
+		//一次读取一个字符数组
+		/*char[] chs = new char[1024];
+		int len;
+		while((len = br.read(chs)) != -1) {
+			bw.write(chs,0,len);
+		}*/
+		
+		//一次读取一个字符
+		/*int ch;
+		while((ch = br.read()) != -1) {
+			bw.write(ch);
+		}*/
+		
+		br.close();
+		bw.close();
+	}
 }
